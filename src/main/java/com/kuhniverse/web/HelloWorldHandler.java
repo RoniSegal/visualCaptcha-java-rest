bb9b671cfa20fe347f5f4572461aaa7ee16d99e2
sealightsci <sealightsci@gmail.com>
2017-08-10 21:47:06 +0300

    Update HelloWorldHandler.java

--- src/main/java/com/kuhniverse/web/HelloWorldHandler.java
+++ src/main/java/com/kuhniverse/web/HelloWorldHandler.java
@@ -2,6 +2,7 @@ package com.kuhniverse.web;
  
 public class HelloWorldHandler {
 	public String getHello() {
+		int b=1;
 		try{
 			for (int i=0; i < 20; i++) {
 				System.out.println("HelloWorldHandler thread: " + Thread.currentThread().getId());
