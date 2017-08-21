a92479c060731c8a3c11000dd129eaeb47358388
nadavye <nadav@sealights.io>
2017-08-21 18:53:19 +0300

    Update tests to use plain phantomjs

--- src/test/java/com/kuhniverse/selenium/SealightsUiTest.java
+++ src/test/java/com/kuhniverse/selenium/SealightsUiTest.java
@@ -5,29 +5,45 @@ import io.ddavison.conductor.Locomotive;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-@Config(
-        browser = Browser.PHANTOMJS,
-        url = "http://localhost:8080/"
-)
-public class SealightsUiTest extends Locomotive {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.ArrayList;
+
+
+public class SealightsUiTest {
+    WebDriver driver;
     @Before
     public void setup() {
+        ArrayList<String> cliArgsCap = new ArrayList<String>();
+        DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+        cliArgsCap.add("--web-security=false");
+        cliArgsCap.add("--ssl-protocol=any");
+        cliArgsCap.add("--ignore-ssl-errors=true");
+        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"phantomjs.exe");
+        driver = new PhantomJSDriver(capabilities);
+        driver.get("http://localhost:8080/");
         //this.driver.manage().window().maximize();
     }
     @Test
     public void testSayHello() throws InterruptedException {
         Thread.sleep(10*1000);
-         click("#say-hello");
+        driver.findElement(By.cssSelector("#say-hello")).click();
+
         Thread.sleep(15*1000);
-        String text = getText("#output");
+        String text = driver.findElement(By.cssSelector("#output")).getText();
         Assert.assertEquals("Hello World", text);
     }
     @Test
     public void testDoMagic() throws InterruptedException {
         Thread.sleep(10*1000);
-        click("#do-magic");
+        driver.findElement(By.cssSelector("#do-magic")).click();
         Thread.sleep(15*1000);
-        String text = getText("#output");
+        String text = driver.findElement(By.cssSelector("#output")).getText();
         Assert.assertEquals("Do Magic", text);
     }
 
