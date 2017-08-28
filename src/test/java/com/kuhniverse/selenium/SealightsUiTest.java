8bf8ddaf69c26a1959b68def38064634cb179314
nadavye <nadav@sealights.io>
2017-08-28 16:30:53 +0300

    Try to debug phantomJS

--- src/test/java/com/kuhniverse/selenium/SealightsUiTest.java
+++ src/test/java/com/kuhniverse/selenium/SealightsUiTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 public class SealightsUiTest {
     PhantomJSDriver driver;
+
     @Before
     public void setup() throws InterruptedException {
         ArrayList<String> cliArgsCap = new ArrayList<String>();
@@ -27,26 +28,54 @@ public class SealightsUiTest {
         capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomExe);
         driver = new PhantomJSDriver(capabilities);
         driver.get("http://localhost:8124/");
-        driver.wait(20*1000);
+        String debugScript  = this.getDebugSscript();
+
+        System.out.println("######################## EXECUTING DEBUG SCRIPT ########################");
+        System.out.println(debugScript);
+        System.out.println("########################################################################");
+        driver.executePhantomJS(debugScript);
     }
+
     @Test
     public void testSayHello() throws InterruptedException {
-        Thread.sleep(10*1000);
+        Thread.sleep(10 * 1000);
         driver.findElement(By.cssSelector("#say-hello")).click();
 
-        Thread.sleep(15*1000);
+        Thread.sleep(15 * 1000);
         String text = driver.findElement(By.cssSelector("#output")).getText();
         Assert.assertEquals("Hello World", text);
     }
+
     @Test
     public void testDoMagic() throws InterruptedException {
-        Thread.sleep(10*1000);
+        Thread.sleep(10 * 1000);
         driver.findElement(By.cssSelector("#do-magic")).click();
-        Thread.sleep(15*1000);
+        Thread.sleep(15 * 1000);
         String text = driver.findElement(By.cssSelector("#output")).getText();
         Assert.assertEquals("Do Magic", text);
     }
 
+    private String getDebugSscript() {
+        String script = "var page = this; var userMessageHelper = {}; userMessageHelper[\"info\"]=console.log; userMessageHelper[\"error\"]=console.error;";
+        script += "page.onConsoleMessage = function (msg, lineNum, sourceId) {\n";
+        script += "  userMessageHelper.info('[Browser] ' + msg);\n";
+        script += "};\n";
+
+        script += "page.onResourceRequested = function (requestData, networkRequest) {\n";
+        script += "  userMessageHelper.info(\"[Resource Request (#'\" + requestData.id + \"')] =====================================================================================\")\n";
+        script += "  userMessageHelper.info('[Resource Request (#' + requestData.id + ')] url: ' + requestData.url);\n";
+        script += "  userMessageHelper.info('[Resource Request (#' + requestData.id + ')] data: ' + JSON.stringify(requestData));\n";
+        script += "  userMessageHelper.info(\"[Resource Request (#'\" + requestData.id + \"')] =====================================================================================\")\n";
+        script += "};\n";
 
 
+        script += "page.onResourceError = function (resourceError) {\n";
+        script += "userMessageHelper.error(\"[Resource Error] =====================================================================================\")\n";
+        script += "userMessageHelper.error('[Resource Error] Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');\n";
+        script += "userMessageHelper.error('[Resource Error] Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);\n";
+        script += "userMessageHelper.error(\"[Resource Error] =====================================================================================\")\n";
+        script += "};\n";
+
+        return script;
+    }
 }
