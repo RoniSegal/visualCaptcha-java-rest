ac94acf5fe1b4471852227223622770678ef22dc
nadavye <nadav@sealights.io>
2017-08-30 12:14:41 +0300

    Update the tests

--- src/test/java/com/kuhniverse/selenium/SealightsUiTest.java
+++ src/test/java/com/kuhniverse/selenium/SealightsUiTest.java
@@ -1,4 +1,5 @@
 package com.kuhniverse.selenium;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,50 +12,59 @@ import java.util.ArrayList;
 
 
 public class SealightsUiTest {
-    PhantomJSDriver driver;
+    String baseUrl = "http://localhost:8080/";
 
-    @Before
-    public void setup() throws InterruptedException {
+
+    private PhantomJSDriver createDriver() {
         ArrayList<String> cliArgsCap = new ArrayList<String>();
         DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
         cliArgsCap.add("--web-security=false");
         cliArgsCap.add("--ssl-protocol=any");
         cliArgsCap.add("--ignore-ssl-errors=true");
         cliArgsCap.add("--local-to-remote-url-access=true");
-        cliArgsCap.add("--remote-debugger-port=12345");
-        cliArgsCap.add("--remote-debugger-autorun=yes");
+        //cliArgsCap.add("--remote-debugger-port=12345");
+        //cliArgsCap.add("--remote-debugger-autorun=yes");
+        //cliArgsCap.add("--proxy==127.0.0.1:8888");
+
         capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
         String phantomExe = "phantomjs.exe";
         if (OSValidator.isUnix())
             phantomExe = "phantomjs";
         capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomExe);
-        driver = new PhantomJSDriver(capabilities);
-        driver.get("http://localhost:8124/");
-        String debugScript  = this.getDebugSscript();
-
-        System.out.println("######################## EXECUTING DEBUG SCRIPT ########################");
-        System.out.println(debugScript);
-        System.out.println("########################################################################");
-        driver.executePhantomJS(debugScript);
+        PhantomJSDriver driver = new PhantomJSDriver(capabilities);
+        return driver;
     }
 
     @Test
     public void testSayHello() throws InterruptedException {
-        Thread.sleep(100 * 1000);
-        driver.findElement(By.cssSelector("#say-hello")).click();
+        PhantomJSDriver driver = createDriver();
+        driver.get(baseUrl);
+        try {
+            Thread.sleep(10 * 1000);
+            driver.findElement(By.cssSelector("#say-hello")).click();
 
-        Thread.sleep(15 * 1000);
-        String text = driver.findElement(By.cssSelector("#output")).getText();
-        Assert.assertEquals("Hello World", text);
+            Thread.sleep(15 * 1000);
+            String text = driver.findElement(By.cssSelector("#output")).getText();
+            Assert.assertEquals("Hello World", text);
+        } finally {
+            driver.quit();
+        }
+
     }
 
     @Test
     public void testDoMagic() throws InterruptedException {
-        Thread.sleep(100 * 1000);
-        driver.findElement(By.cssSelector("#do-magic")).click();
-        Thread.sleep(15 * 1000);
-        String text = driver.findElement(By.cssSelector("#output")).getText();
-        Assert.assertEquals("Do Magic", text);
+        PhantomJSDriver driver = createDriver();
+        driver.get(baseUrl);
+        try {
+            Thread.sleep(10 * 1000);
+            driver.findElement(By.cssSelector("#do-magic")).click();
+            Thread.sleep(15 * 1000);
+            String text = driver.findElement(By.cssSelector("#output")).getText();
+            Assert.assertEquals("Do Magic", text);
+        } finally {
+            driver.quit();
+        }
     }
 
     private String getDebugSscript() {
